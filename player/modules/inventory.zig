90a149c34c2b486d3f06eec1e9b8d7adfd64473a
flye <flyewic@gmail.com>
2025-10-02 16:46:35 +0200

    Added unit tests to inventory.

--- player/modules/inventory.zig
+++ player/modules/inventory.zig
@@ -7,7 +7,7 @@ pub const QuantityOverflow = error.QuantityOverflow;
 
 const Item = struct {
     name: []const u8,
-    quantity: u8 = 1,
+    quantity: u16 = 1,
 };
 
 const Node = struct {
@@ -113,3 +113,77 @@ pub const Inventory = struct {
         self.head = null;
     }
 };
+
+test "adding and stacking items" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    var allocator = gpa.allocator();
+
+    var inv = Inventory.init(&allocator);
+    try inv.add(.{ .name = "Potion", .quantity = 1 });
+    try inv.add(.{ .name = "Potion", .quantity = 2 });
+
+    const potion = try inv.find("Potion");
+    try std.testing.expect(potion.*.quantity == 3);
+
+    inv.deinit();
+}
+
+test "overflowing quantity" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    var allocator = gpa.allocator();
+
+    var inv = Inventory.init(&allocator);
+
+    try inv.add(.{ .name = "Potion", .quantity = 200 });
+
+    const err = inv.add(.{ .name = "Potion", .quantity = 100 }) catch |e| e;
+    try std.testing.expect(err == QuantityOverflow);
+
+    inv.deinit();
+}
+
+test "removing items from empty inventory" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    var allocator = gpa.allocator();
+
+    var inv = Inventory.init(&allocator);
+
+    const err = inv.remove("NonexistentItem") catch |e| e;
+    try std.testing.expect(err == InventoryEmpty);
+
+    inv.deinit();
+}
+
+test "removing item not found" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    var allocator = gpa.allocator();
+
+    var inv = Inventory.init(&allocator);
+
+    try inv.add(.{ .name = "Potion", .quantity = 1 });
+
+    const err = inv.remove("Sword") catch |e| e;
+    try std.testing.expect(err == ItemNotFound);
+
+    inv.deinit();
+}
+
+test "successful remove" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    var allocator = gpa.allocator();
+
+    var inv = Inventory.init(&allocator);
+    try inv.add(.{ .name = "Potion", .quantity = 1 });
+
+    try inv.remove("Potion");
+
+    const err = inv.find("Potion") catch |e| e;
+    try std.testing.expect(err == error.ItemNotFound or err == error.InventoryEmpty);
+
+    inv.deinit();
+}
